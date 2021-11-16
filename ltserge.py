88c46d84a351919a129535c9e19bc91e0236bf88
Serge Urakhchin <sergeurakhchin@gmail.com>
2021-11-15 23:08:10 -0800

    ltserge.py added for dev

--- ltserge.py
+++ ltserge.py
@@ -6,21 +6,25 @@ clean_now = now.strftime("%Y-%b-%d, %A %I:%M:%S")
 message = "Commit made on: "
 full = message + clean_now
 
-working_tree_dir = '/home/ec2-user/crontGit/coding-is-cool'
+working_tree_dir = '/home/ec2-user/crontGit/ltserge.github.io'
 
-file = "coding-is-cool/activityTracker.txt"
+file = "ltserge.github.io/pf24h.txt"
 
 COMMIT_MESSAGE = full
 
 
 def alter_file(file):
     with open(file, "a") as f:
-        f.write(full + "\n")
+
+        # read a list of lines into data
+        data = file.readlines()
+        data[38] = full + "\n"
+        f.write(data)
 
 
 def git_push():
     try:
-        repo = Repo('/home/ec2-user/crontGit/coding-is-cool')
+        repo = Repo('/home/ec2-user/crontGit/ltserge.github.io')
         repo.git.add(update=True)
         repo.index.commit(COMMIT_MESSAGE)
         origin = repo.remote(name='origin')
